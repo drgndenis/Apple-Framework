e5729d014046846d88de9e98e7882c43cdc0beed
drgndenis <serkis-denis.dragan@ogr.iuc.edu.tr>
2023-10-09 16:18:40 +0300

    Initial Commit

---
+++ Apple-Frameworks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apple-Frameworks
+//
+//  Created by Denis DRAGAN on 9.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
